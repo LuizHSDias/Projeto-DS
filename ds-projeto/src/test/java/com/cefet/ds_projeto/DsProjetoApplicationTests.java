52bbeb4d7a00051b47fd423c6f4929ca780322eb
LuizHenrique968 <Luizhenriquemc15@gmail.com>
2025-06-12 11:55:57 -0300

    primeiro commit

---
+++ ds-projeto/src/test/java/com/cefet/ds_projeto/DsProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cefet.ds_projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
