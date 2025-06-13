d0b190caaa3a5d4bf7f63a87c2a426e805c5df24
LuizHenrique968 <Luizhenriquemc15@gmail.com>
2025-06-12 22:23:07 -0300

    correção

--- ds-projeto/src/main/java/com/cefet/ds_projeto/services/DespesaService.java
+++ ds-projeto/src/main/java/com/cefet/ds_projeto/services/DespesaService.java
@@ -4,11 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.cefet.ds_projeto.dto.DespesaDTO;
-import com.cefet.ds_projeto.dto.UsuarioDTO;
 import com.cefet.ds_projeto.entities.Despesa;
-import com.cefet.ds_projeto.entities.Usuario;
 import com.cefet.ds_projeto.repositories.DespesaRepository;
 
 import jakarta.persistence.EntityNotFoundException;
