a9493be594685f889530a57aeb431893e20afab0
LuizHenrique968 <Luizhenriquemc15@gmail.com>
2025-06-12 21:51:08 -0300

    DespesaDTO

--- ds-projeto/src/main/java/com/cefet/ds_projeto/dto/DespesaDTO.java
+++ ds-projeto/src/main/java/com/cefet/ds_projeto/dto/DespesaDTO.java
@@ -1,5 +1,58 @@
 package com.cefet.ds_projeto.dto;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import com.cefet.ds_projeto.entities.Despesa;
+
 public class DespesaDTO {
 
+    private Long id;
+    private String descricao;
+    private LocalDate dataVencimento;
+    private LocalDate dataPagamento;
+    private String situacao;
+    private BigDecimal valor;
+
+    public DespesaDTO() {
+
+    }
+
+    public DespesaDTO(Despesa despesa){
+        this.id = despesa.getId();
+        this.descricao = despesa.getDescricao();
+        this.dataVencimento = despesa.getDataVencimento();
+        this.dataPagamento = despesa.getDataPagamento();
+        this.situacao = despesa.getSituacao();
+        this.valor = despesa.getValor();
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public LocalDate getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public LocalDate getDataPagamento() {
+        return dataPagamento;
+    }
+
+    public String getSituacao() {
+        return situacao;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    
+
+    
 }
