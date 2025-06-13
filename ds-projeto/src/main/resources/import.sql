10999cf052cc1112672a51bd623205ea2e86582a
LuizHenrique968 <Luizhenriquemc15@gmail.com>
2025-06-12 21:20:09 -0300

    sql - dados

--- ds-projeto/src/main/resources/import.sql
+++ ds-projeto/src/main/resources/import.sql
@@ -1,1 +1,7 @@
-INSERT INTO tb_usuario(nome, email, login, senha) VALUES ('Luiz', 'Luiz@gmail.com', 'Luiz132', 'OD6jdnc@!');
+INSERT INTO tb_usuario(nome, email, login, senha) VALUES ('Luiz Henrique', 'Luiz@gmail.com', 'Luiz', 'OD6jdnc@!');
+
+INSERT INTO tb_usuario(nome, email, login, senha) VALUES ('Pedro Lucas', 'Pedro@gmail.com', 'Pedro', 'OD6jdnc@!');
+
+INSERT INTO tb_usuario(nome, email, login, senha) VALUES ('Maria Helena', 'Maria@gmail.com', 'Maria', 'OD6jdnc@!');
+
+INSERT INTO tb_usuario(nome, email, login, senha) VALUES ('José Germano', 'Jose@gmail.com', 'Jose',  'OD6jdnc@!');
