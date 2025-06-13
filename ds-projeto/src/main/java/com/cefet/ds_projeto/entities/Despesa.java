cd25f717c76fc605499df12681ead0e77bab1432
LuizHenrique968 <Luizhenriquemc15@gmail.com>
2025-06-12 21:32:37 -0300

    Classe Despesa

---
+++ ds-projeto/src/main/java/com/cefet/ds_projeto/entities/Despesa.java
@@ -0,0 +1,146 @@
+package com.cefet.ds_projeto.entities;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_despesa")
+public class Despesa {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; 
+
+    private String descricao;
+
+    private LocalDate dataVencimento;
+
+    private LocalDate dataPagamento;
+
+    private String situacao;
+
+    private BigDecimal valor;
+
+    public Despesa() {
+
+    }
+
+    public Despesa(Long id, String descricao, LocalDate dataVencimento, LocalDate dataPagamento, String situacao,
+            BigDecimal valor) {
+        this.id = id;
+        this.descricao = descricao;
+        this.dataVencimento = dataVencimento;
+        this.dataPagamento = dataPagamento;
+        this.situacao = situacao;
+        this.valor = valor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(LocalDate dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public LocalDate getDataPagamento() {
+        return dataPagamento;
+    }
+
+    public void setDataPagamento(LocalDate dataPagamento) {
+        this.dataPagamento = dataPagamento;
+    }
+
+    public String getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(String situacao) {
+        this.situacao = situacao;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
+        result = prime * result + ((dataVencimento == null) ? 0 : dataVencimento.hashCode());
+        result = prime * result + ((dataPagamento == null) ? 0 : dataPagamento.hashCode());
+        result = prime * result + ((situacao == null) ? 0 : situacao.hashCode());
+        result = prime * result + ((valor == null) ? 0 : valor.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Despesa other = (Despesa) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (descricao == null) {
+            if (other.descricao != null)
+                return false;
+        } else if (!descricao.equals(other.descricao))
+            return false;
+        if (dataVencimento == null) {
+            if (other.dataVencimento != null)
+                return false;
+        } else if (!dataVencimento.equals(other.dataVencimento))
+            return false;
+        if (dataPagamento == null) {
+            if (other.dataPagamento != null)
+                return false;
+        } else if (!dataPagamento.equals(other.dataPagamento))
+            return false;
+        if (situacao == null) {
+            if (other.situacao != null)
+                return false;
+        } else if (!situacao.equals(other.situacao))
+            return false;
+        if (valor == null) {
+            if (other.valor != null)
+                return false;
+        } else if (!valor.equals(other.valor))
+            return false;
+        return true;
+    }
+}
