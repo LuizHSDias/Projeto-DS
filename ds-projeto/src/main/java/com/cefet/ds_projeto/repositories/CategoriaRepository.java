914b50713a30b3ffb767fdb2d2335871657f64cd
marcelafm19 <marcelafmariano@gmail.com>
2025-06-16 08:54:53 -0300

    Categoria e Receitas (controllers e services)

--- ds-projeto/src/main/java/com/cefet/ds_projeto/repositories/CategoriaRepository.java
+++ ds-projeto/src/main/java/com/cefet/ds_projeto/repositories/CategoriaRepository.java
@@ -1,11 +1,13 @@
 package com.cefet.ds_projeto.repositories;
 
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.cefet.ds_projeto.entities.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-    List<Categoria> findByTipo(String tipo);
-    
-    List<Categoria> findByDescricaoContaining(String descricao);
+    List<Categoria> findByTipoContainingIgnoreCase(String tipo);
+    boolean existsByDescricao(String descricao);
 }
--- ds-projeto/src/main/java/com/cefet/ds_projeto/repositories/ReceitaRepository.java
+++ ds-projeto/src/main/java/com/cefet/ds_projeto/repositories/ReceitaRepository.java
@@ -1,12 +1,15 @@
 package com.cefet.ds_projeto.repositories;
 
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.cefet.ds_projeto.entities.Receita;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
 public interface ReceitaRepository extends JpaRepository<Receita, Long> {
-   
-    List<Receita> findByCategoriaId(Long categoriaId);
-    
     List<Receita> findByUsuarioId(Long usuarioId);
+    List<Receita> findByCategoriaId(Long categoriaId);
+    List<Receita> findByDataEntradaBetween(LocalDate dataInicio, LocalDate dataFim);
 }
