46ac1905e621ee06e2ed4d779ca0fa089d16178a
Luiz Henrique <Luizhenriquemc15@gmail.com>
2025-06-24 20:04:19 -0300

    Delete Desktop directory

--- Desktop/Projeto-DS-main/Projeto-DS-main/ds-projeto/src/main/java/com/cefet/ds_projeto/repositories/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cefet.ds_projeto.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cefet.ds_projeto.entities.Usuario;
-
-public interface UsuarioRepository extends JpaRepository <Usuario, Long> {
-    boolean existsByLogin(String login);
-}
