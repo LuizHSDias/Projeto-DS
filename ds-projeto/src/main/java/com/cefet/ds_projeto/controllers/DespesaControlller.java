bff80777cec1343f456a2564cca991b4cb17c44d
LuizHenrique968 <Luizhenriquemc15@gmail.com>
2025-06-26 00:51:49 -0300

    Documentação

--- ds-projeto/src/main/java/com/cefet/ds_projeto/controllers/DespesaControlller.java
+++ ds-projeto/src/main/java/com/cefet/ds_projeto/controllers/DespesaControlller.java
@@ -15,6 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cefet.ds_projeto.dto.DespesaDTO;
 import com.cefet.ds_projeto.services.DespesaService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+
 @RestController
 @RequestMapping("/despesas")
 
@@ -24,43 +27,62 @@ public class DespesaControlller {
 	
 	
 	@GetMapping("/{id}")
-	public ResponseEntity<DespesaDTO> findById(@PathVariable Long id) {
+	@Operation(summary = "Buscar despesa por ID", description = "Retorna os dados de uma despesa específica com base no seu ID.")
+	public ResponseEntity<DespesaDTO> findById(
+		@Parameter(description = "ID da despesa", example = "2")
+		@PathVariable Long id) {
 	DespesaDTO despesaDTO = despesaService.findById(id);
 	return ResponseEntity.ok(despesaDTO);
 	}
 	
 	@GetMapping
+	@Operation(summary = "Listar todas as despesas", description = "Retorna todas as despesas cadastradas no sistema.")
 	public ResponseEntity<List<DespesaDTO>> findAll() {
 	List<DespesaDTO> despesasDTOs = despesaService.findAll();
 	return ResponseEntity.ok(despesasDTOs);
 	}
 	
 	@PostMapping
+	@Operation(summary = "Cadastrar nova despesa", description = "Cria uma nova despesa no sistema.")
 	public ResponseEntity<DespesaDTO> create(@RequestBody DespesaDTO despesaDTO) {
 	DespesaDTO novoDespesa = despesaService.insert(despesaDTO);
 	return ResponseEntity.status(201).body(novoDespesa);
 	}
 	
 	@PutMapping("/{id}")
-	public ResponseEntity<DespesaDTO> update(@PathVariable Long id, @RequestBody DespesaDTO despesaDTO) {
+	@Operation(summary = "Atualizar despesa", description = "Atualiza os dados de uma despesa existente com base no ID.")
+	public ResponseEntity<DespesaDTO> update(
+		@Parameter(description = "ID da despesa a ser atualizada", example = "10")
+		@PathVariable Long id,
+		@Parameter(description = "Novos dados da despesa") 
+		@RequestBody DespesaDTO despesaDTO) {
 	DespesaDTO despesaAtualizado = despesaService.update(id, despesaDTO);
 	return ResponseEntity.ok(despesaAtualizado);
 	}
 	
 	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> delete(@PathVariable Long id) {
+	@Operation(summary = "Excluir despesa", description = "Remove uma despesa do sistema com base no ID.")
+	public ResponseEntity<Void> delete(
+		@Parameter(description = "ID da despesa a ser excluída", example = "10")
+		@PathVariable Long id) {
 	despesaService.delete(id);
 	return ResponseEntity.noContent().build();
 	}
 
 	@GetMapping("/usuario/{usuarioId}")
-    public ResponseEntity<List<DespesaDTO>> findByUsuarioId(@PathVariable Long usuarioId) {
+	@Operation(summary = "Listar despesas por usuário", description = "Retorna todas as despesas associadas a um usuário específico.")
+    public ResponseEntity<List<DespesaDTO>> findByUsuarioId(
+		@Parameter(description = "ID do usuário", example = "3")
+		@PathVariable Long usuarioId) {
         List<DespesaDTO> despesas = despesaService.findByUsuarioId(usuarioId);
         return ResponseEntity.ok(despesas);
     }
     
     @GetMapping("/categoria/{categoriaId}")
-    public ResponseEntity<List<DespesaDTO>> findByCategoriaId(@PathVariable Long categoriaId) {
+	@Operation(summary = "Listar despesas por categoria", description = "Retorna todas as despesas associadas a uma categoria específica.")
+    public ResponseEntity<List<DespesaDTO>> findByCategoriaId(
+		@Parameter(description = "ID do usuário", example = "5")
+		@PathVariable Long categoriaId) {
         List<DespesaDTO> despesas = despesaService.findByCategoriaId(categoriaId);
         return ResponseEntity.ok(despesas);
     }
--- ds-projeto/src/main/java/com/cefet/ds_projeto/controllers/UsuarioController.java
+++ ds-projeto/src/main/java/com/cefet/ds_projeto/controllers/UsuarioController.java
@@ -17,6 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cefet.ds_projeto.dto.UsuarioDTO;
 import com.cefet.ds_projeto.services.UsuarioService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+
 @RestController
 @RequestMapping("/usuarios")
 
@@ -27,43 +30,62 @@ public class UsuarioController {
 	
 	
 	@GetMapping("/{id}")
-	public ResponseEntity<UsuarioDTO> findById(@PathVariable Long id) {
+	@Operation(summary = "Buscar Usuário por ID", description = "Retorna os dados de um usuário específico.")
+	public ResponseEntity<UsuarioDTO> findById(
+		@Parameter(description = "ID do usuário a ser buscado", example = "1")
+		@PathVariable Long id) {
 	UsuarioDTO usuarioDTO = usuarioService.findById(id);
 	return ResponseEntity.ok(usuarioDTO);
 	}
 	
 	@GetMapping
+	@Operation(summary = "Listar todos os usuários", description = "Retorna a lista de todos os usuários cadastrados.")
 	public ResponseEntity<List<UsuarioDTO>> findAll() {
 	List<UsuarioDTO> usuariosDTOs = usuarioService.findAll();
 	return ResponseEntity.ok(usuariosDTOs);
 	}
 	
 	@PostMapping
-	public ResponseEntity<UsuarioDTO> create(@RequestBody UsuarioDTO usuarioDTO) {
+	@Operation(summary = "Criar um novo usuário", description = "Adiciona um novo usuário ao sistema.")
+	public ResponseEntity<UsuarioDTO> create(
+		@Parameter(description = "ID do usuário a ser atualizado", example = "1") 
+        @PathVariable Long id,
+        @Parameter(description = "Dados atualizados do usuário")
+		@RequestBody UsuarioDTO usuarioDTO) {
 	UsuarioDTO novoUsuario = usuarioService.insert(usuarioDTO);
 	return ResponseEntity.status(201).body(novoUsuario);
 	}
 	
 	@PutMapping("/{id}")
+	 @Operation(summary = "Atualizar usuário", description = "Atualiza os dados de um usuário existente com base no ID.")
 	public ResponseEntity<UsuarioDTO> update(@PathVariable Long id, @RequestBody UsuarioDTO usuarioDTO) {
 	UsuarioDTO usuarioAtualizado = usuarioService.update(id, usuarioDTO);
 	return ResponseEntity.ok(usuarioAtualizado);
 	}
 	
 	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> delete(@PathVariable Long id) {
+	@Operation(summary = "Excluir usuário", description = "Remove um usuário do sistema com base no ID.")
+	public ResponseEntity<Void> delete(
+		@Parameter(description = "ID do usuário a ser excluído", example = "1")
+		@PathVariable Long id) {
 	usuarioService.delete(id);
 	return ResponseEntity.noContent().build();
 	}
 	
 	@GetMapping("/existe-email")
-	public ResponseEntity<Boolean> emailExiste(@RequestParam String email){
+	@Operation(summary = "Verificar existência de e-mail", description = "Verifica se um determinado e-mail já está cadastrado no sistema.")
+	public ResponseEntity<Boolean> emailExiste(
+		@Parameter(description = "E-mail a ser verificado", example = "usuario@email.com")
+		@RequestParam String email){
 		Boolean resultado = usuarioService.emailExiste(email);
 		return ResponseEntity.ok(resultado);
 	}
 
 	@GetMapping("/existe-login")
-	public ResponseEntity<Boolean> loginExiste(@RequestParam String login){
+	@Operation(summary = "Verificar existência de login", description = "Verifica se um determinado login já está cadastrado no sistema.")
+	public ResponseEntity<Boolean> loginExiste(
+		@Parameter(description = "Login a ser verificado", example = "usuario123")
+		@RequestParam String login){
 		Boolean resultado = usuarioService.loginExiste(login);
 		return ResponseEntity.ok(resultado);
 	}
