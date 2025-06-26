ec4942156c98e2a82dd6df76e942be900a32beae
LuizHenrique968 <Luizhenriquemc15@gmail.com>
2025-06-26 00:55:54 -0300

    Link Documentação

--- ds-projeto/src/main/java/com/cefet/ds_projeto/DsProjetoApplication.java
+++ ds-projeto/src/main/java/com/cefet/ds_projeto/DsProjetoApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DsProjetoApplication {
 
+	// DOCUMENTAÇÃO PROJETO 
+	// http://localhost:8080/swagger-ui/index.html#/
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DsProjetoApplication.class, args);
 	}
